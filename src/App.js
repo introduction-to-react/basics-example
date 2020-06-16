527296aac49d16cfcd0b7007970d64e613fc613d
Michael König <koenigmichael2002@gmail.com>
2020-06-16 09:58:33 +0200

    Add several examples

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import NavbarItem from "./components/navbar-item"
+import Counter from "./components/counter"
+import MyInput from "./components/my-input"
+import ConditionalRendering from "./components/conditional-rendering"
+import Iterating from "./components/iterating"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <NavbarItem href="/" color="blue">
+          Home
+        </NavbarItem>
+        <NavbarItem href="/about" color="red">
+          about
+        </NavbarItem>
+        <NavbarItem href="/contact" color="green">
+          contact
+        </NavbarItem>
+      </div>
+      <br />
+      <Counter />
+      <br />
+      <MyInput />
+      <br />
+      <ConditionalRendering />
+      <br />
+      <Iterating />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
